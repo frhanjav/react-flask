74a1662c48b5b47afc18d14a9864dfec408ff451
Farhan Javed <farhan.javed006@gmail.com>
2024-10-04 14:06:45 +0530

    returning new created user in routes

--- backend/routes.py
+++ backend/routes.py
@@ -15,11 +15,12 @@ def create_friend():
   try:
     data = request.json
 
-    required_fields = ["name", "role", "description", "gender"]
+    # Validations
+    required_fields = ["name","role","description","gender"]
     for field in required_fields:
-      if field not in data:
+      if field not in data or not data.get(field):
         return jsonify({"error":f'Missing required field: {field}'}), 400
-    
+
     name = data.get("name")
     role = data.get("role")
     description = data.get("description")
@@ -38,7 +39,7 @@ def create_friend():
     db.session.add(new_friend) 
     db.session.commit()
 
-    return jsonify({"msg":"Friend created successfully"}),201
+    return jsonify(new_friend.to_json()), 201
     
   except Exception as e:
     db.session.rollback()
